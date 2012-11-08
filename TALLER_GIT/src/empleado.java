7a672dec8c0c99dc4d0cf56419c490651df4c008
Juan David Ospina Vasquez <juandaosp12@gmail.com>
2012-11-08 00:11:49 -0500

    Agregado Empleado.java

--- TALLER_GIT/src/empleado.java
+++ TALLER_GIT/src/empleado.java
@@ -5,8 +5,125 @@
 
 /**
  *
- * @author HOME
+ * @author Juan David Ospina
  */
 public class empleado {
+    
+    int  salario;
+    String nombre, login, contrasena , eps ,id, licencia ,genero, 
+            estado_civil, fecha_nac ,cargo, estado;
+
+    public empleado(){}
+    
+    public void setSalario(int salario){
+    
+        this.salario = salario;
+    }
+    public void setNombre(String nombre){
+    
+        this.nombre = nombre;
+    }
+    public void setLogin(String login){
+    
+        this.login = login;
+    }
+    public void setContrasena(String contrasena){
+    
+        this.contrasena = contrasena;
+    }
+    public void setEps(String eps){
+    
+        this.eps = eps;
+    }
+    public void setId(String id){
+    
+        this.id = id;
+    }
+    public void setLicencia(String licencia){
+    
+        this.licencia = licencia;
+    }
+    public void setGenero(String genero){
+    
+        this.genero = genero;
+    }
+    public void setEstado_civil(String ec){
+    
+        this.estado_civil = ec;
+    }
+    public void setFecha_nac(String fecha_nac){
+    
+        this.fecha_nac = fecha_nac;
+    }
+    public void setCargo(String cargo){
+    
+        this.cargo = cargo;
+    }
+    public void setEstado(String estado){
+    
+        this.estado = estado;
+    }
+    
+    public int getSalario(){
+    
+        return this.salario;
+    }
+    public String getNombre(){
+    
+        return this.nombre;
+    }
+    public String getLogin(){
+    
+        return this.login;
+    }
+    public String getContrasena(){
+    
+        return this.contrasena;
+    }
+    public String getEps(){
+    
+        return this.eps;
+    }
+    public String getId(){
+    
+        return this.id;
+    }
+    public String getLicencia(){
+    
+        return this.licencia;
+    }
+    public String getGenero(){
+    
+        return this.genero;
+    }
+    public String getEstado_civil(){
+    
+        return this.estado_civil;
+    }
+    
+    public String getFecha_nac(){
+    
+        return this.fecha_nac;
+    }
+    public String getCargo(){
+    
+        return this.cargo;
+    }
+    
+    public String getEstado(){
+    
+        return this.estado;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
     
 }
